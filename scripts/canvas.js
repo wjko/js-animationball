14b97af5ae1a84f2ee574db2a71e9229a2fe420e
Wenjeng Ko <owen.ko.owen@gmail.com>
2018-09-15 20:33:38 +0800

    add image and drawing code
    Added image loading and arc, rect, text, line drawings in the code.

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,9 +1,57 @@
-var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// var c = document.querySelector('canvas');
+var c = document.getElementById("myCanvas");
+c.width = window.innerWidth;
+c.height = window.innerHeight/2;
+var ctx = c.getContext('2d');
 
-var c = canvas.getContext('2d');
-c.fillRect(0, 0, 100, 100);
-c.fillRect(100, 100, 100, 100);
+ctx.globalCompositeOperation='destination-over';
 
-console.log(canvas);
+// Draw image
+// window.onload = function() {
+//     var img = document.getElementById("lights");
+//     ctx.drawImage(img, 0, 0, c.width, c.height);
+// }
+var bgImage = new Image();
+bgImage.src = "images/img_lights.jpg";
+bgImage.onload = function() {
+    ctx.drawImage(bgImage, 0, 0, c.width, c.height);
+}
+
+ctx.strokeStyle = "white"; // '#bbbbbb';
+
+// Declare the variables
+var radius = 100;
+var startAngle = 1;
+var endAngle = 2;
+var incrementAngle = 0.1;
+var centerX = c.width/2;
+var centerY = c.height/2;
+
+// Draw the center point
+ctx.fillRect(centerX, centerY, 1, 1);
+
+// Draw arc
+ctx.beginPath();
+ctx.arc(centerX, centerY, radius, startAngle*Math.PI, endAngle*Math.PI);
+
+// Draw lines
+for (let angle = startAngle; angle < endAngle; angle += incrementAngle) {
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(centerX+radius*Math.cos(angle*Math.PI), centerY+radius*Math.sin(angle*Math.PI));
+}
+
+// Draw top part circles
+var startAngle = 0;
+var endAngle = 1;
+var incrementRadius = 20;
+for (let r = radius; r >= 20; r -= incrementRadius) {
+    ctx.arc(centerX, centerY, r, startAngle*Math.PI, endAngle*Math.PI);
+}
+
+ctx.stroke();
+
+// Draw text at the bottom of the canvas
+ctx.font = "30px Comic Sans MS";
+ctx.fillStyle = 'yellow';
+ctx.textAlign = "center";
+ctx.fillText("JavaScript 研習營", centerX, c.height - 10);
